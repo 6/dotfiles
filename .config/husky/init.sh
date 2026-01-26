434db35ba251170839eeedf12848baf882d62506
Peter Graham <peterghm@gmail.com>
2026-01-26 09:58:23 -0500

    new husky support

---
+++ .config/husky/init.sh
@@ -0,0 +1,4 @@
+# Fixes "bun command not found" and similar issues for Husky in GUI apps
+# https://typicode.github.io/husky/troubleshoot.html
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="$HOME/.local/share/mise/shims:$PATH"
